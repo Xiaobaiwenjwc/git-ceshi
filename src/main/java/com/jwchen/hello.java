bd96cbf55dc471f16b177d06b2058a83b99e024a
xiaobaiwen <chenjiawen660@163.com>
2024-05-25 20:57:52 +0800

    hot-fix commit 2

--- src/main/java/com/jwchen/hello.java
+++ src/main/java/com/jwchen/hello.java
@@ -8,5 +8,6 @@ public class hello {
         System.out.println("hello git!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
+        System.out.println("hot-fix git1!");
     }
 }
